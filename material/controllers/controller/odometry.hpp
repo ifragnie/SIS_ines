c4fba44bc2757dac6b5ff17d5970df13113097f6
ifragnie <ines.fragniere@epfl.ch>
2026-05-25 14:17:27 +0200

    Add debug output for odometry calculations
    Added debug print statement for wheel distances and velocities.

--- material/controllers/controller/odometry.hpp
+++ material/controllers/controller/odometry.hpp
@@ -53,6 +53,75 @@ void compute_odometry(double*wheel_rot, double dt, double*v, double*w, double* d
     *distL_out = dLeft  * R;
     *distR_out = dRight * R;
 
+    printf("dL=%.6f  dR=%.6f  leftW=%.6f  rightW=%.6f\n", dLeft, dRight, left_wheel, right_wheel);
+
+    //convert to linear dist
+    double distL=dLeft*R;
+    double distR=dRight*R;
+
+    //velocities
+
+    *v = (*distR_out + *distL_out) / (2.0 * dt);
+    *w = (*distR_out - *distL_out) / (L   * dt);
+    
+    //update
+    prev_left = left_wheel;
+    prev_right = right_wheel;
+}#pragma once 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <memory.h>
+
+#include "pioneer_interface/pioneer_interface.hpp"
+
+#define RAD2DEG(X)      X / M_PI * 180.0 // Convert radians to degrees
+
+inline double wrap_angle(double a){
+
+    while(a > M_PI)  a -= 2.0 * M_PI;
+    while(a < -M_PI) a += 2.0 * M_PI;
+
+    return a;
+}
+
+//robot parameter 
+void compute_odometry(double*wheel_rot, double dt, double*v, double*w, double* distL_out, double* distR_out){
+    //Robot size
+    const double R = PioneerInfo::wheel_radius;
+    const double L = PioneerInfo::axis_length;
+
+    if (dt < 1e-9) {
+        *v = 0; *w = 0; *distL_out = 0; *distR_out = 0;
+        return;
+    }
+
+    //Static to keep track of previous wheel rotations
+    static double prev_left =0.0;
+    static double prev_right=0.0;
+    static bool first= true;
+
+    //New wheel rotations
+    double left_wheel = wheel_rot[0];
+    double right_wheel = wheel_rot[1];
+
+    // if it is the first time running ?
+    if (first){
+        prev_left=left_wheel;
+        prev_right=right_wheel;
+        first=false;
+        *v=0; *w=0; *distL_out = 0; *distR_out = 0;
+        return;
+    }
+
+    //change in wheel motion
+    double dLeft=left_wheel - prev_left;
+    double dRight=right_wheel - prev_right;
+
+    *distL_out = dLeft  * R;
+    *distR_out = dRight * R;
+
     //convert to linear dist
     double distL=dLeft*R;
     double distR=dRight*R;
